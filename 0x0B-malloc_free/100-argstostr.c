1e09e9df858442bf71d1d57d20a4bc94cb320826
Kngset <kngset247@gmail.com>
2023-01-11 03:03:29 -0800

    Update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,6 +17,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
+		
 		for (n = 0; av[i][n]; n++)
 			l++;
 
@@ -30,7 +31,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 	for (n = 0; av[i][n]; n++)
-	}
+	{
 		str[r] = av[i][n];
 		r++;
 	}
